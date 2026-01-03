005bd12a1e3efc0acaf10a3a46e53a4b91104f1e
Murli Goel <murli.goel1999@gmail.com>
2026-01-03 11:17:12 +0000

    added controller layer for task manager

---
+++ task-manager/src/main/java/com/example/taskmanager/task_manager/controller/TaskManagementController.java
@@ -0,0 +1,9 @@
+package com.example.taskmanager.task_manager.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskManagementController {
+    
+    
+}
